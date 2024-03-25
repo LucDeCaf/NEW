81f735acb3148c6804a2b176f9a9bf8fc2c3567d
LucDeCaf <ldecafmeyer@gmail.com>
2024-03-25 10:21:07 +0200

    finished tokeniser + added framework for ast

---
+++ input.md
@@ -0,0 +1,9 @@
+###### This is a heading
+
+> This is a cool little blockquote
+> with paragraph continuation text
+
+This is just a paragraph
+
+-   and this is list item 1
+-   and list item 2
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,45 @@
 use std::{env, fs};
 
 #[derive(Debug, PartialEq)]
-enum Block {
-    Document(Vec<Block>),
-    BlockQuote(Vec<Block>),
-    List(Vec<Block>),
+enum Token {
+    BlockQuote,
+    Whitespace,
+    List { delim: String, ordered: bool },
+    Heading(usize),
     Paragraph(String),
+}
+
+#[derive(Debug)]
+enum HTMLElement {
+    Document,
+    Heading {
+        level: usize,
+    },
+    OrderedList {
+        delim: String,
+        tight: bool,
+        start: usize,
+    },
+    UnorderedList {
+        delim: String,
+        tight: bool,
+    },
+    ListItem,
+    Paragraph,
+    BlockQuote,
+    Emphasis,
+    Strong,
+    CodeBlock {
+        language: String,
+    },
+    Text(String),
+}
+
+#[derive(Debug)]
+struct Node {
+    element: HTMLElement,
+    children: Option<Vec<Token>>,
+    open: bool,
 }
 
 fn remove_indicator(line: &str) -> String {
@@ -20,24 +54,79 @@ fn remove_indicator(line: &str) -> String {
     }
 }
 
+// fn deepest_open_node_mut<'a>(root: &'a mut Node) -> Option<&'a mut Node> {
+//     if !root.open {
+//         return None;
+//     }
+
+//     let mut traveller = root;
+
+//     while let Some(traveller.children)
+
+//     traveller
+// }
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    if args.len() != 2 {
-        panic!("invalid argument count");
-    }
+    // ? Removed mandatory file input for debugging purposes
+    // ? Instead using default file path of "./input.md"
+    let filepath = match args.len() {
+        2 => &args[1],
+        _ => "./input.md",
+    };
 
-    let file_contents = fs::read_to_string(&args[1]).unwrap();
+    let file_contents = fs::read_to_string(filepath).unwrap();
 
-    let mut document = Block::Document(vec![]);
+    let mut tokens: Vec<Token> = vec![];
 
     for line in file_contents.lines() {
-        let line = line.trim();
+        let mut line = line.trim().to_owned();
 
-        if line.starts_with(">") {
-            
+        // Whitespace
+        if line.len() == 0 {
+            tokens.push(Token::Whitespace);
+            continue;
         }
+
+        // Blockquote
+        if line.starts_with(">") {
+            tokens.push(Token::BlockQuote);
+            line = remove_indicator(&line);
+        }
+
+        // List
+        if line.starts_with("-") {
+            tokens.push(Token::List {
+                delim: line.chars().next().unwrap().to_string(),
+                ordered: false,
+            });
+            line = remove_indicator(&line);
+        }
+
+        // Heading
+        let hashless = line.trim_start_matches('#');
+        let hash_count = line.len() - hashless.len();
+
+        if hash_count > 0 && hash_count < 7 {
+            tokens.push(Token::Heading(hash_count));
+            line = hashless[1..].to_owned();
+        }
+
+        // Paragraph
+        tokens.push(Token::Paragraph(line));
     }
 
-    dbg!(&document);
+    dbg!("Tokens: ", &tokens);
+
+    // let mut document = Node {
+    //     element: HTMLElement::Document,
+    //     children: Some(vec![]),
+    //     open: true,
+    // };
+
+    // for token in tokens {
+    //     let last_open = deepest_open_node_mut(&mut document);
+
+    // }
 }
